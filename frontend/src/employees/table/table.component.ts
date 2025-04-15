41acce9cbe5957aff3af161f0093794f7aed5314
DimDob <dimitardobrev.business@gmail.com>
2025-04-16 01:08:16 +0300

    add employees table

--- frontend/src/employees/table/table.component.ts
+++ frontend/src/employees/table/table.component.ts
@@ -1,42 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
 import { Employee } from '../model/employee.model';
 import { CommonModule } from '@angular/common';
+import { EmployeeService } from '../service/employee.service';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: Employee[] = [
-  {
-    id: '1',
-    name: 'Alice',
-    surname: 'Johnson',
-    department: 'Engineering',
-    salary: '$100k',
-    salary_amount: 100000,
-    email: 'alice.johnson@example.com',
-    employee_phone_number: '123-456-7890'
-  },
-  {
-    id: '2',
-    name: 'Bob',
-    surname: 'Smith',
-    department: 'Marketing',
-    salary: '$85k',
-    salary_amount: 85000,
-    email: 'bob.smith@example.com',
-    employee_phone_number: '234-567-8901'
-  },
-];
-
-
-/**
- * @title Basic use of `<table mat-table>`
- */
 
 @Component({
   selector: 'employee-table',
@@ -45,7 +12,23 @@ const ELEMENT_DATA: Employee[] = [
   templateUrl: 'table.component.html',
   styleUrl: 'table.component.css',
 })
-export class TableComponent {
+export class TableComponent implements OnInit {
+
+  apiService = inject(EmployeeService);
+
+  public ELEMENT_DATA: Employee[] = [];
+
+
+  ngOnInit(): void {
+    this.apiService.getAllEmployees().subscribe({
+      next: (res) => {
+        this.ELEMENT_DATA.push(...res);
+      },
+      error: (err) => console.error('Error fetching employee:', err)
+    });
+  }
+
+
   displayedColumns: string[] = [
     'id',
     'name',
@@ -57,5 +40,5 @@ export class TableComponent {
     'employee_phone_number'
   ];
 
-  dataSource = ELEMENT_DATA;
+  dataSource = this.ELEMENT_DATA;
 }
