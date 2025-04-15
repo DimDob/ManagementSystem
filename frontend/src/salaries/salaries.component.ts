3680424b89da3a925d17691ed8eb1ee2c6b7c4c9
DimDob <dimitardobrev.business@gmail.com>
2025-04-16 01:22:01 +0300

    add table to salaries as well

--- frontend/src/salaries/salaries.component.ts
+++ frontend/src/salaries/salaries.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, inject, OnInit} from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+import { CommonModule } from '@angular/common';
+import { Employee } from '../employees/model/employee.model';
+import { EmployeeService } from '../employees/service/employee.service';
+
 
 @Component({
-  selector: 'app-salaries',
-  imports: [],
-  templateUrl: './salaries.component.html',
-  styleUrl: './salaries.component.css'
+  selector: 'employee-table',
+  standalone: true,
+  imports: [CommonModule, MatTableModule],
+  templateUrl: 'salaries.component.html',
+  styleUrl: 'salaries.component.css',
 })
-export class SalariesComponent {
+export class SalariesComponent implements OnInit {
 
+  apiService = inject(EmployeeService);
+
+  public ELEMENT_DATA: Employee[] = [];
+
+  cdr = inject(ChangeDetectorRef);
+
+  ngOnInit(): void {
+    this.apiService.getAllEmployees().subscribe({
+      next: (res) => {
+        this.dataSource = res;
+        this.cdr.detectChanges();
+      },
+      error: (err) => console.error('Error fetching employee:', err)
+    });
+  }
+
+  displayedColumns: string[] = [
+    'name',
+    'surname',
+    'department',
+    'salary',
+    'salary_amount'
+  ];
+
+  dataSource = this.ELEMENT_DATA;
 }
