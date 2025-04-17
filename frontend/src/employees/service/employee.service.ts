49fab3d03f3347206daa7a49ce205d8853f5e4a9
DimDob <dimitardobrev.business@gmail.com>
2025-04-17 09:44:03 +0300

    remove comment

--- frontend/src/employees/service/employee.service.ts
+++ frontend/src/employees/service/employee.service.ts
@@ -1,5 +1,3 @@
-// src/app/services/employees.service.ts
-
 import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
