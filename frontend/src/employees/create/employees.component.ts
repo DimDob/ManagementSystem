97f01ddee01b919996af15d3b2a76b1da1f3ff44
DimDob <dimitardobrev.business@gmail.com>
2025-04-28 11:14:45 +0300

    optimize imports

--- frontend/src/employees/create/employees.component.ts
+++ frontend/src/employees/create/employees.component.ts
@@ -1,14 +1,11 @@
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { EmployeeService } from '../service/employee.service';
-import { Employee } from '../model/employee.model';
 import { FormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
 import { EditComponent } from "../edit/edit/edit.component";
-import { SharedService } from '../service/shared.service';
 import { BaseEmployeeComponent } from '../../base-employee/base-employee.component';
 
 @Component({
